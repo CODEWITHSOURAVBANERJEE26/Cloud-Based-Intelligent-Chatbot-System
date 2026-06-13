ec5da660938471a7259dc5ed5882a89ee0f0be18
CODEWITHSOURAVBANERJEE26 <147583018+CODEWITHSOURAVBANERJEE26@users.noreply.github.com>
2026-06-13 12:15:14 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>
+Cloud-Based Intelligent Chatbot System
+</title>
+
+<link rel="stylesheet"
+href="style.css">
+
+</head>
+
+<body>
+
+<h1>
+Cloud-Based Intelligent Chatbot System
+</h1>
+
+<form action="chat.php"
+method="POST">
+
+<input
+type="text"
+name="message"
+placeholder="Ask a question"
+required>
+
+<br><br>
+
+<input
+type="submit"
+value="Send">
+
+</form>
+
+</body>
+</html>
